bfd295b47c06b5a37376758cc9bf1e92015affef
Gaurab Dutta <gaurabdutta8@gmail.com>
2021-03-26 15:25:02 +0530

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
@@ -7,9 +7,9 @@ model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('form.html')
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
@@ -20,7 +20,8 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('form.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
